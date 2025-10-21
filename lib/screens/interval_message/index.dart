f428b4086710b2ba5388bda7eb97f3cb440cffbc
emotionalboySY <gs_k_bear17@naver.com>
2025-10-22 08:31:04 +0900

    feat: add daily message

--- lib/screens/interval_message/index.dart
+++ lib/screens/interval_message/index.dart
@@ -2,8 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import '../../controllers/controller_interval_message.dart';
 import '../../models/model_interval_message.dart';
+import 'create_daily.dart';
 import 'create_exact.dart';
 import 'create_weekly.dart';
+import 'detail_daily.dart';
 import 'detail_exact.dart';
 import 'detail_weekly.dart';
 
@@ -27,6 +29,7 @@ class IntervalMessageScreen extends GetView<IntervalMessageController> {
             tabs: [
               Tab(text: '정확한 시간'),
               Tab(text: '요일 시간'),
+              Tab(text: '매일'), // 추가
             ],
             labelStyle: TextStyle(
               letterSpacing: -1.5,
@@ -43,6 +46,7 @@ class IntervalMessageScreen extends GetView<IntervalMessageController> {
           children: [
             _buildExactTimeTab(),
             _buildWeeklyTab(),
+            _buildDailyTab(), // 추가
           ],
         ),
         floatingActionButton: FloatingActionButton(
@@ -51,8 +55,10 @@ class IntervalMessageScreen extends GetView<IntervalMessageController> {
             final tabIndex = controller.selectedTabIndex.value;
             if (tabIndex == 0) {
               Get.to(() => CreateExactTimeMessageScreen());
-            } else {
+            } else if (tabIndex == 1) {
               Get.to(() => CreateWeeklyMessageScreen());
+            } else {
+              Get.to(() => CreateDailyMessageScreen()); // 추가
             }
           },
           child: Icon(Icons.add),
@@ -129,6 +135,43 @@ class IntervalMessageScreen extends GetView<IntervalMessageController> {
           itemBuilder: (context, index) {
             final message = messages[index];
             return _buildWeeklyMessageCard(message);
+          },
+        ),
+      );
+    });
+  }
+
+  Widget _buildDailyTab() {
+    return Obx(() {
+      if (controller.isLoading.value && controller.dailyMessages.isEmpty) {
+        return Center(child: CircularProgressIndicator());
+      }
+
+      final messages = controller.filteredDailyMessages;
+
+      if (messages.isEmpty) {
+        return Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(Icons.today, size: 64, color: Colors.grey),
+              SizedBox(height: 16),
+              Text(
+                '등록된 메시지가 없습니다.',
+                style: TextStyle(fontSize: 18, color: Colors.grey),
+              ),
+            ],
+          ),
+        );
+      }
+
+      return RefreshIndicator(
+        onRefresh: () => controller.loadDailyMessages(),
+        child: ListView.builder(
+          itemCount: messages.length,
+          itemBuilder: (context, index) {
+            final message = messages[index];
+            return _buildDailyMessageCard(message);
           },
         ),
       );
@@ -307,7 +350,87 @@ class IntervalMessageScreen extends GetView<IntervalMessageController> {
     );
   }
 
-  void _showDeleteDialog(String id, String message, {required bool isExact}) {
+  Widget _buildDailyMessageCard(DailyMessage message) {
+    return Card(
+      margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+      elevation: 2,
+      child: InkWell(
+        onTap: () {
+          Get.to(() => DailyMessageDetailScreen(message: message));
+        },
+        child: Padding(
+          padding: EdgeInsets.all(16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                children: [
+                  Icon(
+                    Icons.today,
+                    color: message.isActive ? Colors.orange : Colors.grey,
+                  ),
+                  SizedBox(width: 8),
+                  Expanded(
+                    child: Text(
+                      message.formattedTime,
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                        color: message.isActive ? Colors.black : Colors.grey,
+                      ),
+                    ),
+                  ),
+                  Switch(
+                    value: message.isActive,
+                    onChanged: (value) {
+                      if (message.id != null) {
+                        controller.toggleDailyMessageActive(message.id!);
+                      }
+                    },
+                  ),
+                ],
+              ),
+              SizedBox(height: 8),
+              Text(
+                message.message,
+                style: TextStyle(
+                  fontSize: 14,
+                  color: message.isActive ? Colors.black87 : Colors.grey,
+                ),
+                maxLines: 2,
+                overflow: TextOverflow.ellipsis,
+              ),
+              SizedBox(height: 8),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  TextButton.icon(
+                    onPressed: () {
+                      Get.to(() => DailyMessageDetailScreen(message: message));
+                    },
+                    icon: Icon(Icons.edit, size: 18),
+                    label: Text('수정'),
+                  ),
+                  SizedBox(width: 8),
+                  TextButton.icon(
+                    onPressed: () => _showDeleteDialog(
+                      message.id!,
+                      message.message,
+                      isDaily: true,
+                    ),
+                    icon: Icon(Icons.delete, size: 18, color: Colors.red),
+                    label: Text('삭제', style: TextStyle(color: Colors.red)),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _showDeleteDialog(String id, String message, {bool isExact = false, bool isDaily = false}) {
     Get.dialog(
       AlertDialog(
         title: Text('메시지 삭제'),
@@ -322,6 +445,8 @@ class IntervalMessageScreen extends GetView<IntervalMessageController> {
               Get.back();
               if (isExact) {
                 controller.deleteExactTimeMessage(id);
+              } else if (isDaily) {
+                controller.deleteDailyMessage(id); // 추가
               } else {
                 controller.deleteWeeklyMessage(id);
               }
